50a097f032516bea362b43410a00342478ff0267
Gustavo A. Verneck <gustavoverneck@gmail.com>
2024-01-08 14:40:37 -0300

    Final edition

--- legendador.py
+++ legendador.py
@@ -7,17 +7,18 @@ from deep_translator import GoogleTranslator
 
 print("### Python Subtitle Generator")
 
-lang_from="ja"
-lang_to="pt"
+lang_from=input("\n Por favor insira o código da língua de origem: ")
+lang_to=input("\n Por favor insira o código da língua que deseja: ")
+video_name = input("\n Insira o nome do arquivo de vídeo com a extensão: ")
 
 print("Converting MP4 -> MP3 ...")
 ###     MP4-MP3 converter
-video = moviepy.editor.VideoFileClip("video.mp4")
+video = moviepy.editor.VideoFileClip(video_name)
 video.audio.write_audiofile("audio.mp3")
 
 print("Transcribing...")
 ###     Transcribring
-aai.settings.api_key = "f1cee072b30e4e98af78d0773bee0017"
+aai.settings.api_key = "INSERT HERE YOUT ASSEMBLYAI SEED"
 
 # Adicionar o código do idioma
 config = aai.TranscriptionConfig(language_code=lang_from)
@@ -28,9 +29,10 @@ transcript = transcriber.transcribe("./audio.mp3")
 
 #print(transcript.export_subtitles_srt())
 
-with open("legenda-ja.srt", "w") as f:
+with open("legenda-{}}.srt".format(lang_from), "w") as f:
     f.write(transcript.export_subtitles_srt())
 f.close()
+
 print("Translating...")
 ###     Translating
 legenda = transcript.export_subtitles_srt().splitlines()
@@ -41,12 +43,10 @@ while (i <= len(legenda)):
     legenda[i] = GoogleTranslator(source=lang_from, target=lang_to).translate(legenda[i])
     i += 4
 
-with open("legenda-pt.srt", "w") as f2:
+with open("legenda-{}.srt".format(lang_to), "w") as f2:
     for i in legenda:
         f2.write(i)
         f2.write("\n")
 
-
 print("Done!")
-#translated = GoogleTranslator(source=lang_from, target=lang_to).translate_file('./legenda-{}.srt'.format(lang_from))
 
